5cdd6e2a69d23ee521a5ac242e29dd7d07cf8ab6
Your Name <your-email@example.com>
2024-10-22 12:26:26 +0530

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 // import Home from './components/Home';
 // import Contact from './components/Contact';
 // import Apply from './components/Apply';
