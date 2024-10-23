c6f11b1ec598760f65cd539626902ec7b4f631bb
Your Name <your-email@example.com>
2024-10-23 11:52:02 +0530

    spme change

--- src/Components/navbar/Telicart.jsx
+++ src/Components/navbar/Telicart.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { FaRupeeSign } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
+import { FaRegStarHalfStroke } from "react-icons/fa6";
+
+
+
+const data = [
+  {
+    id: 1,
+    image: 'https://assets.sangeethamobiles.com/product_img/16314/1712572187_lqh.jpg',
+    name: 'Xiaomi Smart TV X Series GTV 43 (108 cm)',
+    price: '₹22999.00',
+    delivery: 'Free delivery'
+  },
+  {
+    id: 1,
+    image: 'https://assets.sangeethamobiles.com/product_img/16314/1712572187_lqh.jpg',
+    name: 'Xiaomi Smart TV X Series GTV 43 (108 cm)',
+    price: '₹22999.00',
+    delivery: 'Free delivery'
+  },
+  {
+    id: 1,
+    image: 'https://assets.sangeethamobiles.com/product_img/16314/1712572187_lqh.jpg',
+    name: 'Xiaomi Smart TV X Series GTV 43 (108 cm)',
+    price: '₹22999.00',
+    delivery: 'Free delivery'
+  },
+  {
+    id: 1,
+    image: 'https://assets.sangeethamobiles.com/product_img/16314/1712572187_lqh.jpg',
+    name: 'Xiaomi Smart TV X Series GTV 43 (108 cm)',
+    price: '₹22999.00',
+    delivery: 'Free delivery'
+  },
+];
 
 const Telicart = () => {
   return (
-    <div className='Television ' >
-        
-    </div>
-  )
-}
+    <div className='Television'>
+     
+        {data.map((item) => (
+          <div key={item.id} className="telecart">
+            <img src={item.image} alt="Television" />
+            <div className="content">
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+            <p id="startv">  <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaStar />
+              <FaRegStarHalfStroke /></p>
 
-export default Telicart
+    
+
+
+              <p id="delivery">{item.delivery} <button id="btncart">ADD TO CART</button></p>
+            </div>
+          </div>
+        ))}
+   
+    </div>
+  );
+};
+
+export default Telicart;
